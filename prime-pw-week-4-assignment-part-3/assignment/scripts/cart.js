3352905fb2b27bd7260a88fab129719b5e8a4f54
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-01-08 19:31:27 -0600

    Add files via upload
    finished the assignment.

--- prime-pw-week-4-assignment-part-3/assignment/scripts/cart.js
+++ prime-pw-week-4-assignment-part-3/assignment/scripts/cart.js
@@ -2,12 +2,11 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket= [];
-let empty = [];
 
 function addItem(item){
-  console.log("added", item, "to the basket");
   //push new item into Basket
   basket.push(item);
+  console.log("added", item, "to the basket");
   return  true;
 }
 console.log(addItem());
@@ -16,3 +15,9 @@ addItem("poodle")
 addItem("horse")
 
 console.log(basket, 'in the basket');
+
+function empty(){
+  basket.splice(0, basket.length);
+}//empty the basket.
+empty(basket)
+console.log("Did I empty the basket?", basket);
